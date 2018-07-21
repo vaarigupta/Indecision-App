8209cdeb2e8d9e5917aa37314f3f388dcfe84f7b
vaarigupta <vaarigupta24@gmail.com>
2018-07-21 23:56:49 +0530

    2 tempaltes created

--- public/script/app.js
+++ public/script/app.js
@@ -1,12 +1,48 @@
-"use strict";
+'use strict';
 
 console.log("React");
 var template = React.createElement(
-  "p",
-  null,
-  "hey Loki"
+	'div',
+	null,
+	React.createElement(
+		'p',
+		null,
+		'hey Loki '
+	),
+	React.createElement(
+		'p',
+		null,
+		' I dont know why'
+	)
+);
+
+var template2 = React.createElement(
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		null,
+		'Hellloo'
+	),
+	React.createElement(
+		'p',
+		null,
+		' Name : vaari '
+	),
+	React.createElement(
+		'p',
+		null,
+		' Age : 21 '
+	),
+	React.createElement(
+		'p',
+		null,
+		' Location : Delhi '
+	)
 );
 
 var approot = document.getElementById('root');
+var approot1 = document.getElementById('root1');
 
 ReactDOM.render(template, approot);
+ReactDOM.render(template2, approot1);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 console.log("React");
-var template = <p>hey Loki</p>;
+var template = (
+	   <div>
+		<p>hey Loki </p>
+		<p> I dont know why</p>
+	   </div>
+			   );
+
+var template2 = (
+<div> 
+<h1>Hellloo</h1>
+<p> Name : vaari </p>
+<p> Age : 21 </p>
+<p> Location : Delhi </p>
+</div>
+)			   
 
 var approot = document.getElementById('root');
+var approot1 = document.getElementById('root1');
 
 ReactDOM.render(template,approot)
+ReactDOM.render(template2,approot1)
